0e29f24829e03ee4c827f753863461dcf51a4635
hashimhashmi02 <hashimhashmi02@gmail.com>
2025-11-24 23:59:29 +0530

    added button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Toggle } from "@radix-ui/react-toggle";
+import { Ghost } from "lucide-react";
 
 const Page = ()=>{
 return (
-  <div>
-    <Button>
-      <Toggle>
-Click Me  
-    
-      </Toggle>
+  <div>      
+      <Button variant={"link"}>
+      Click Me     
     </Button>
-      
-
-    
+    <Button variant={"outline"}>
+      Click Me     
+    </Button>
+    <Button variant={"secondary"}>
+      Click Me     
+    </Button>
+    <Button variant={"destructive"}>
+      Click Me     
+    </Button>
+    <Button variant={"default"}>
+      Click Me     
+    </Button>   
     <div>
     </div>
     </div>
