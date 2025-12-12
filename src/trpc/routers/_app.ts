0209ac28602c7cf88189bf1e9a076203be24e81c
hashimhashmi02 <hashimhashmi02@gmail.com>
2025-12-12 23:43:52 +0530

    projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,9 @@
 import { createTRPCRouter } from '../init';
-import { messageRouter } from '@/modules/messages/server/procedures';
+import { projectsRouter } from '@/modules/projects/server/procedures';
+import { messageRouter } from '@/modules/messages/procedures';
 
 export const appRouter = createTRPCRouter({
   messages : messageRouter,
+  projects: projectsRouter
 });
 export type AppRouter = typeof appRouter;
